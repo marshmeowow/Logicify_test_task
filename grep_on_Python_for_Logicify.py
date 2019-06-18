70540888d41067632c57928bbcaa548fb6d060f7
marshmeowow <maximilion28@gmail.com>
2019-06-18 11:13:38 +0300

    add main function

--- grep_on_Python_for_Logicify.py
+++ grep_on_Python_for_Logicify.py
@@ -1,5 +1,6 @@
 import re
 import argparse
+import sys
 
 
 def grep(subline, file, is_invert):
@@ -25,3 +26,12 @@ def args_lookup(args):
     parser.add_argument('subline', action="store", help='Define a subline')
     parser.add_argument('file_name', action="store", help='Define a file name')
     return parser.parse_args(args)
+
+
+def main():
+    params = args_lookup(sys.argv[1:])
+    grep(params.subline, params.file_name, params.invert)
+
+
+if __name__ == '__main__':
+    main()
