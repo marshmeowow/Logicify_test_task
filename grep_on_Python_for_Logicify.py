0470b1e1e16395f8b7eadb791ce40de9d2d13b36
marshmeowow <maximilion28@gmail.com>
2019-06-18 11:11:11 +0300

    add args parser

--- grep_on_Python_for_Logicify.py
+++ grep_on_Python_for_Logicify.py
@@ -1,4 +1,5 @@
 import re
+import argparse
 
 
 def grep(subline, file, is_invert):
@@ -9,3 +10,18 @@ def grep(subline, file, is_invert):
                 print(line.rstrip())
             elif not match and is_invert:
                 print(line.rstrip())
+
+
+def args_lookup(args):
+    parser = argparse.ArgumentParser(description='GREP on Python')
+
+    parser.add_argument(
+        '-v',
+        action="store_true",
+        dest="invert",
+        default=False,
+        help='Selecte lines are that do NOT match')
+
+    parser.add_argument('subline', action="store", help='Define a subline')
+    parser.add_argument('file_name', action="store", help='Define a file name')
+    return parser.parse_args(args)
